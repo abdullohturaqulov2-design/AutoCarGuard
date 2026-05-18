964c676b28efdc73aec3907fc3173e64a8278f7f
Abdulloh <abdullohturaqulov2@gmail.com>
2026-05-18 07:56:42 -0700

    Inshaalloh bu loyiham ham zo'r chiqadi

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -8,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
